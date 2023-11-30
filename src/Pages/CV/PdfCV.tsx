e8e2d3611005d360e0c55d96b43e14ffacc542dc
MariusNWK <marius.nowak@epitech.eu>
2023-11-30 22:29:22 +0100

    feat: set site language to fr + new CV.jpg + modify CV height

--- src/Pages/CV/PdfCV.tsx
+++ src/Pages/CV/PdfCV.tsx
@@ -34,7 +34,7 @@ export default function PdfCV() {
 }
 
 const StyledImage = styled.img`
-  height: 80vh;
+  height: calc(100vh - 200px);
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 30px;
 `;
